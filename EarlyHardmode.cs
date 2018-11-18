804e48ea779b09bc8d19acf9c95069057d86b1f8
valkyrienyanko <sebastianbelle074@gmail.com>
2018-11-18 14:46:36 -0600

    commit

---
+++ EarlyHardmode.cs
@@ -0,0 +1,16 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EarlyHardmode
+{
+	class EarlyHardmode : Mod
+	{
+		public EarlyHardmode()
+		{
+            
+            //NPC.NewNPC(0, 0, NPCID.WallofFlesh);
+
+        }
+	}
+}
---
+++ HardmodeWorld.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EarlyHardmode
+{
+    class HardmodeWorld : ModWorld
+    {
+        public override void Initialize()
+        {
+            if (!Main.hardMode)
+            {
+                WorldGen.StartHardmode();
+            }
+        }
+    }
+}
